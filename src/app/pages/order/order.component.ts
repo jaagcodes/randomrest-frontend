262f931abe8f6d697161215003cd0dc6d9d4133c
Jose Alejandro <jose.arrauttg@pragma.com.co>
2024-06-11 11:34:51 -0500

    bug: add paginator

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Order } from '../../_model/order';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
@@ -8,36 +8,37 @@ import { OrderService } from '../../_service/order.service';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
   dataSource: MatTableDataSource<Order>;
   displayedColumns: string[] = ['id', 'dish', 'status', 'createdAt', 'updatedAt'];
   cantidad: number = 0;
+  
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(
-    private orderService: OrderService
-  ){}
+  constructor(private orderService: OrderService) {}
 
   ngOnInit(): void {
     this.orderService.listPurchaseHistory(1, 10).subscribe(data => {
       this.cantidad = data.total;
       this.dataSource = new MatTableDataSource(data.data);
+      this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
   }
 
-  filter(e : any){
-    this.dataSource.filter = e.target.value.trim().toLowerCase();
+  filter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  mostrarMas(e:any){
-    let pageIndex = e.pageIndex + 1;
-    this.orderService.listPurchaseHistory(pageIndex, e.pageSize).subscribe(data => {
+  mostrarMas(event: any) {
+    let pageIndex = event.pageIndex + 1;
+    this.orderService.listPurchaseHistory(pageIndex, event.pageSize).subscribe(data => {
       this.cantidad = data.total;
-      this.dataSource = new MatTableDataSource(data.data);
-    })
+      this.dataSource.data = data.data;
+    });
   }
 }
